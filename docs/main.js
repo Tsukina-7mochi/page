d599370d88caf18482eea1a147fb7f2844cc2bd6
Tsukina-mochi <Tsukina7mochi@gmail.com>
2021-05-10 00:22:39 +0900

    add en lang

--- docs/main.js
+++ docs/main.js
@@ -3,7 +3,13 @@ import "./style.scss";
 import addListener from './clickImage.js';
 
 window.addEventListener('load', () => {
-  document.querySelectorAll('#illustrations img').forEach((el) => {
+  document.querySelectorAll('.illustrations img').forEach((el) => {
     addListener(el);
+  });
+
+  document.querySelectorAll('.toggleLang').forEach((el) => {
+    el.addEventListener('click', () => {
+      document.querySelector('main').dataset.lang = el.dataset.set;
+    });
   });
 });
